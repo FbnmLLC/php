37a506bda5511b020545ddfa14d33bea7ee64fda
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-06-25 16:59:12 +0430

    Create PHP Sample Code

---
+++ create.php
@@ -0,0 +1,15 @@
+<?php
+include_once('functions.php');
+$merchant_id = '';
+$amount = '';
+$callback_url = '';
+$response = create($merchant_id, $amount, $callback_url);
+$result = json_decode($response);
+if ($result->status) {
+    header('Location:' . $result->payment_url);
+} else {
+    echo '<pre>';
+    print_r($result);
+    echo '<pre>';
+}
+?>
---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+function create($merchant_id, $amount, $callback_url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://123pay.ir/api/v1/create/payment');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "merchant_id=$merchant_id&amount=$amount&callback_url=$callback_url");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function verify($merchant_id, $RefNum)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://123pay.ir/api/v1/verify/payment');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "merchant_id=$merchant_id&RefNum=$RefNum");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
