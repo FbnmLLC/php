5fd33fa219d90acdb5fd09926275d7e1d2a8ee24
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-06-27 12:15:26 +0430

    Update Code Style

--- verify.php
+++ verify.php
@@ -1,19 +1,19 @@
 <?php
-include_once('functions.php');
+include_once( 'functions.php' );
 $merchant_id = '';
-$State = $_REQUEST['State'];
-$RefNum = $_REQUEST['RefNum'];
-if ($State == 'OK') {
-    $response = verify($merchant_id, $RefNum);
-    $result = json_decode($response);
-    if ($result->status) {
-        echo 'تراکنش موفق';
-    } else {
-        echo '<pre>';
-        print_r($result);
-        echo '<pre>';
-    }
+$State       = $_REQUEST['State'];
+$RefNum      = $_REQUEST['RefNum'];
+if ( $State == 'OK' ) {
+	$response = verify( $merchant_id, $RefNum );
+	$result   = json_decode( $response );
+	if ( $result->status ) {
+		echo 'تراکنش موفق';
+	} else {
+		echo '<pre>';
+		print_r( $result );
+		echo '<pre>';
+	}
 } else {
-    echo 'تراکنش ناموفق - نیازی به ارسال متد وریفای نیست';
+	echo 'تراکنش ناموفق - نیازی به ارسال متد وریفای نیست';
 }
 ?>
