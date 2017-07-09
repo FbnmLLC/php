54c364b9b9ef420645375832ca2de13801cb4544
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-07-09 14:25:46 +0430

    Update callback_url

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 include_once( 'functions.php' );
 $merchant_id  = '';
 $amount       = '';
-$callback_url = '';
+$callback_url = urlencode( 'http://domain.ltd' );
 $response     = create( $merchant_id, $amount, $callback_url );
 $result       = json_decode( $response );
 if ( $result->status ) {
